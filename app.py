ed8a098c9ad090e9582c9af12c39ebfaebd5cdc7
Neil Casas <neilalfonzcasas@gmail.com>
2024-06-12 17:45:17 +0800

    adjusted year filter spacing for constructor standings

--- app.py
+++ app.py
@@ -13,12 +13,13 @@ def index():
 @app.route("/drivers")
 def drivers():
     return render_template("drivers.html")
+
 @app.route("/constructors", methods=["GET", "POST"])
 def constructors(year=2024): 
     if request.method == "POST":
         year = request.form.get("year", 2024, type=int)
     constructor_standings = e.season(year).get_constructor_standing().constructor_standings
-    return render_template("constructors.html", constructor_standings = constructor_standings, year = year)
+    return render_template("constructors.html", constructor_standings = constructor_standings, year = year, selected_year = year)
 
 @app.route("/races")
 def races():
