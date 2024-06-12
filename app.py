dc8bb43e06892c42b455b1a22b285cee014af830
Neil Casas <neilalfonzcasas@gmail.com>
2024-06-12 22:42:38 +0800

    added race results to race results template

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# save this as app.py
 from flask import Flask, render_template, request
 import ergast_py
 
@@ -8,6 +7,7 @@ e = ergast_py.Ergast()
 
 @app.route("/")
 def index():
+    print(e.get_races())
     return render_template("index.html")
 
 @app.route("/drivers", methods=["GET", "POST"])
@@ -15,7 +15,6 @@ def drivers(year=2024):
     if request.method == "POST":
         year = request.form.get("year", 2024, type=int)
     driver_standings = e.season(year).get_driver_standing().driver_standings
-    print(driver_standings)
     return render_template("drivers.html", driver_standings = driver_standings, year = year, selected_year = year)
 
 
@@ -26,6 +25,19 @@ def constructors(year=2024):
     constructor_standings = e.season(year).get_constructor_standing().constructor_standings
     return render_template("constructors.html", constructor_standings = constructor_standings, year = year, selected_year = year)
 
-@app.route("/races")
-def races():
-    return render_template("races.html")
+@app.route("/races", methods=["GET", "POST"])
+def races(year=2024):
+    if request.method == "POST":
+        year = request.form.get("year", 2024, type=int)
+
+    race_results = []
+    # Get results for every circuit for that season
+    season_circuits = e.season(year).get_circuits()
+    
+    # Append to race results list
+    for circuit in season_circuits:
+        try:
+            race_results.append(e.season(year).circuit(circuit.circuit_id).get_result)
+        except:
+            continue
+    return render_template("races.html", race_results = race_results, year = year, selected_year = year)
