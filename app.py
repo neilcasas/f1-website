042c8f4ab1c9712dc9a1f3ea921314ef90faf17d
Neil Casas <neilalfonzcasas@gmail.com>
2024-06-12 17:28:00 +0800

    added constructors standing with filters

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # save this as app.py
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, request
 import ergast_py
 
 app = Flask(__name__)
@@ -13,10 +13,12 @@ def index():
 @app.route("/drivers")
 def drivers():
     return render_template("drivers.html")
-
-@app.route("/constructors")
-def constructors():
-    return render_template("constructors.html")
+@app.route("/constructors", methods=["GET", "POST"])
+def constructors(year=2024): 
+    if request.method == "POST":
+        year = request.form.get("year", 2024, type=int)
+    constructor_standings = e.season(year).get_constructor_standing().constructor_standings
+    return render_template("constructors.html", constructor_standings = constructor_standings, year = year)
 
 @app.route("/races")
 def races():
