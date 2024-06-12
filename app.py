f385ccb54020636a52c9f797870a707136101075
Neil Casas <neilalfonzcasas@gmail.com>
2024-06-12 12:03:23 +0800

    included bootstrap

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 # save this as app.py
-from flask import Flask
+from flask import Flask, render_template, redirect
+import ergast_py
 
 app = Flask(__name__)
 
+e = ergast_py.Ergast()
+
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return render_template("layout.html")
