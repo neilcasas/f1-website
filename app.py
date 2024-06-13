0d80a44d578a097c6fa7140818fcaebd755d344b
Neil Casas <neilalfonzcasas@gmail.com>
2024-06-13 13:37:46 +0800

    added comments as well as a sorting method for the race results

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from operator import itemgetter
 import ergast_py
 import asyncio
 import aiohttp
@@ -30,26 +31,48 @@ def constructors(year=2024):
 def races(year=2024):
     if request.method == "POST":
         year = request.form.get("year", 2024, type=int)
-    
+
     race_results = asyncio.run(get_race_results(year))
-    print(race_results)
+
+    # Convert each 'round' into an int
+    for result in race_results:
+        try:
+            result['round'] = int(result['round'])
+        except ValueError:
+            # Handle cases where 'round' is not a valid integer
+            pass
+    
+    # Sort by round
+    race_results.sort(key=itemgetter('round'))
     
     return render_template("races.html", race_results=race_results, year=year, selected_year=year)
 
+# Method for fetching race result per circuit in a season
 async def fetch_race_result(session, year, circuit_id):
     url = f"https://ergast.com/api/f1/{year}/circuits/{circuit_id}/results.json?limit=5"
+
+    # Access the Ergast API URL and wait for a response
     async with session.get(url) as response:
+        # If response is successful
         if response.status == 200:
+            # Wait for JSON data and return it, if there's no JSON data then return None
             data = await response.json()
             return data['MRData']['RaceTable']['Races'][0] if data['MRData']['RaceTable']['Races'] else None
+        # Return none if request failed
         return None
 
+# Perform the fetch requests concurrently 
 async def get_race_results(year):
     async with aiohttp.ClientSession() as session:
-        tasks = []
-        season_circuits = e.season(year).get_circuits()
+        tasks = [] # List of fetch requests
+        season_circuits = e.season(year).get_circuits() # Get all circuits within that season
+
+        # For every circuit within that season
         for circuit in season_circuits:
+
+            # Create a fetch request for its race result
             tasks.append(fetch_race_result(session, year, circuit.circuit_id))
         
+        # Perform the fetch requests in the tasks list concurrently
         race_results = await asyncio.gather(*tasks)
         return [result for result in race_results if result]
