02e9bb12fa224624124f5323408d459be622f22e
Neil Casas <neilalfonzcasas@gmail.com>
2024-06-13 12:53:51 +0800

    implemented async function in fetching race results per circuit per season

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import ergast_py
+import asyncio
+import aiohttp
 
 app = Flask(__name__)
 
@@ -29,17 +31,25 @@ def constructors(year=2024):
 def races(year=2024):
     if request.method == "POST":
         year = request.form.get("year", 2024, type=int)
-
-    race_results = []
-    # Get results for every circuit for that season
-    season_circuits = e.season(year).get_circuits()
     
-    # Append to race results list
-    for circuit in season_circuits:
-        try:
-            # TODO: Make this faster and sort by year
-            race_results.append(e.season(year).circuit(circuit.circuit_id).get_result())
+    race_results = asyncio.run(get_race_results(year))
+    
+    return render_template("races.html", race_results=race_results, year=year, selected_year=year)
 
-        except:
-            continue
-    return render_template("races.html", race_results = race_results, year = year, selected_year = year)
+async def fetch_race_result(session, year, circuit_id):
+    url = f"https://ergast.com/api/f1/{year}/circuits/{circuit_id}/results.json?limit=5"
+    async with session.get(url) as response:
+        if response.status == 200:
+            data = await response.json()
+            return data['MRData']['RaceTable']['Races'][0] if data['MRData']['RaceTable']['Races'] else None
+        return None
+
+async def get_race_results(year):
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        season_circuits = e.season(year).get_circuits()
+        for circuit in season_circuits:
+            tasks.append(fetch_race_result(session, year, circuit.circuit_id))
+        
+        race_results = await asyncio.gather(*tasks)
+        return [result for result in race_results if result]
