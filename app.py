a45483d5b699cdf95817319f94e232370674d4db
Neil Casas <neilalfonzcasas@gmail.com>
2024-06-19 17:03:56 +0800

    added latest race results to home page

--- app.py
+++ app.py
@@ -34,10 +34,10 @@ def index():
                                           )['articles']
     # Get latest results
     race = e.season().round().get_race()
-    latest_results = e.season().round().get_results()
+    latest_results = e.season().round().get_result()
     print(race  )
     print(latest_results)
-    return render_template("index.html", articles=f1_articles, results=latest_results, race=race)
+    return render_template("index.html", articles=f1_articles, race_results=latest_results, race=race)
 
 # Driver standings
 @app.route("/drivers", methods=["GET", "POST"])
